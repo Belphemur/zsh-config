a18ed5f766636416a904152a1bc02ec8bcd8cfeb
Antoine Aflalo <antoine@aaflalo.me>
2018-10-18 14:26:23 -0400

    Add history arrow UP/DOWN

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -2,3 +2,10 @@
 HISTFILE=~/.zsh_history
 HISTSIZE=30000
 SAVEHIST=25000
+
+
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
