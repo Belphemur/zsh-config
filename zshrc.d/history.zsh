8613d3b27f941bb547ddd294006deaca7665a482
Antoine Aflalo <git@aaflalo.me>
2020-08-07 20:38:22 -0400

    Disable normal history

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -37,8 +37,8 @@ setopt HIST_NO_STORE
 ## Remove addition of executable files by = cmd
 unsetopt EQUALS
 
-autoload -U history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
+#autoload -U history-search-end
+#zle -N history-beginning-search-backward-end history-search-end
+#zle -N history-beginning-search-forward-end history-search-end
+#bindkey "^[[A" history-beginning-search-backward-end
+#bindkey "^[[B" history-beginning-search-forward-end
